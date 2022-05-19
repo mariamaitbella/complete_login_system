5cc3161c36cba1a9f62ef4f4e023ed5e8d7e8993
Mariam Ait Bella <mariam.aitbella200@gmail.com>
2022-05-19 16:55:25 +0100

    submit Button - sub form

---
+++ components/Buttons/RegularButton.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+//styled components
+import styled from "styled-components/native";
+import { colors } from "../colors";
+const { primary, accent } = colors;
+import RegularText from "../Texts/RegularText";
+
+const ButtonView = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${accent};
+  width:100%;
+  justify-content:center;
+  align-items: center;
+  border-radius: 10px;
+  height:60px;
+`;
+
+const RegularButton = (props) => {
+  return(
+  <ButtonView onPress={props.onPress} {...props}>
+    <RegularText style={[{color:primary}, {...props?.textStyle}]}>{props.children}</RegularText>
+  </ButtonView>
+  );
+};
+
+export default RegularButton;
--- screens/Login.js
+++ screens/Login.js
@@ -1,53 +1,90 @@
-import React, {useState} from "react";
-import {Formik} from 'formik';
+import React, { useState } from "react";
+import { Formik } from "formik";
+import { ActivityIndicator } from "react-native";
 //custom components
 import MainContainer from "../components/Containers/MainContainer";
 import KeyboardAvoidingContainer from "../components/Containers/KeyboardAvoidingContainer";
 import RegularText from "../components/Texts/RegularText";
 import StyledTextInput from "../components/Inputs/StyledTextInput";
 import MsgBox from "../components/Texts/MsgBox";
+import RegularButton from "../components/Buttons/RegularButton";
+import { colors } from "../components/colors";
+const { primary } = colors;
 
 const Login = () => {
-    const [message, setMessage]= useState('');
-    const [isSuccessMessage, setisSuccessMessage]= useState(false);
+  const [message, setMessage] = useState("");
+  const [isSuccessMessage, setisSuccessMessage] = useState(false);
+
+  const handleLogin = async (credentials, setSubmiting) => {
+    try {
+      setMessage(null);
+      //backend
+      //move to next page
+    } catch (error) {
+      setMessage("Login failed" + error.message);
+      setSubmitting(false);
+    }
+  };
+
   return (
     <MainContainer>
       <KeyboardAvoidingContainer>
         <RegularText style={{ marginBottom: 25 }}>
           Enter your account credentials
         </RegularText>
-        <Formik initialValues={{email:'', password:''}}>
-            {({handleChange, handleBlur, handleSubmit,values, isSubmitting}) =>(
-                <>
-                <StyledTextInput
-                
-                    label="Email Address"
-                    icon="email-variant"
-                    placeholder="walt4@gmail.com"
-                    keyboardType="email-address"
-                    onChangeText={handleChange('email')}
-                    onBlur={handleBlur('email')}
-                    value={values.email}
-                    style={{marginBottom: 25}}
-
-                    />
-                <StyledTextInput
-                
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            if (values.email == "" || values.password == "") {
+              setMessage("Please fill in all fields");
+              setSubmitting(false);
+            } else {
+              handleLogin(values, setSubmitting);
+            }
+          }}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            isSubmitting,
+          }) => (
+            <>
+              <StyledTextInput
+                label="Email Address"
+                icon="email-variant"
+                placeholder="walt4@gmail.com"
+                keyboardType="email-address"
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+                style={{ marginBottom: 25 }}
+              />
+              <StyledTextInput
                 label="Password"
                 icon="lock-open"
                 placeholder="* * * * * * * *"
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
                 value={values.password}
                 isPassword={true}
-                style={{marginBottom: 25}}
-                />
-                <MsgBox style={{marginBottom: 25}} success={isSuccessMessage}>{message || 'an error here'}</MsgBox>
-                </>
-            )}
-
+                style={{ marginBottom: 25 }}
+              />
+              <MsgBox style={{ marginBottom: 25 }} success={isSuccessMessage}>
+                {message || "an error here"}
+              </MsgBox>
+              {!isSubmitting && (
+                <RegularButton onPress={handleSubmit}>Login</RegularButton>
+              )}
+              {isSubmitting && (
+                <RegularButton disabled={true}>
+                  <ActivityIndicator size="small" color={primary} />
+                </RegularButton>
+              )}
+            </>
+          )}
         </Formik>
-        
       </KeyboardAvoidingContainer>
     </MainContainer>
   );
